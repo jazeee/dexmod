6ba25e13aae8d70c143512dec20c9e4507035a7f
Jaz Singh <jsingh@practichem.com>
2018-12-22 14:59:49 -0800

    Fix build script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,9 +2,10 @@
 
 cd ./base
 apktool b --output ../new/base-apktool.apk
+cd -
 cd ./new
 rm base-aligned.apk
 /opt/android-sdk/build-tools/28.0.3/zipalign -v -p 4 base-apktool.apk base-aligned.apk
 /opt/android-sdk/build-tools/28.0.3/apksigner sign --ks release-key-dexcom.jks --out base.apk base-aligned.apk
 adb install -r base.apk
-
+cd -
